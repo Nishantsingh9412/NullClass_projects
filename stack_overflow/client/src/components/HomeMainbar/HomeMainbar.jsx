fbce7452efe0391ef70b981eb984dae8ea2ad188
nishant <59526404+Nishantsingh9412@users.noreply.github.com>
2023-08-10 01:25:53 +0530

    Homemainbar Update

--- stack_overflow/client/src/components/HomeMainbar/HomeMainbar.jsx
+++ stack_overflow/client/src/components/HomeMainbar/HomeMainbar.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import { Link , useLocation  } from 'react-router-dom';
+
+import './HomeMainbar.css';
 
 const HomeMainbar = () => {
+    
+  var questionsList = [{
+    id: 1,
+    votes:3,
+    noOfAnswers: 2,
+    questionTitte: "What is a function?" ,
+    questionBody: "It meant to be",
+    questionTags: ["java", "node js", "react js", "mongodb"],
+    userPosted:"mano",
+    askedOn: "jan 1",
+
+  },{
+    id: 2,
+    votes:0,
+    noOfAnswers: 2,
+    questionTitte: "What is a function?" ,
+    questionBody: "It meant to be",
+    questionTags: ["java", "node js", "react js", "mongodb"],
+    userPosted:"mano",
+    askedOn: "jan 1"
+  },{ 
+    id: 3,
+    votes:1,
+    noOfAnswers: 0,
+    questionTitte: "What is a function?" ,
+    questionBody: "It meant to be",
+    questionTags: ["java", "node js", "react js", "mongodb"],
+    userPosted:"mano",
+    askedOn: "jan 1"
+  }]
+
+  const location = useLocation()
+
   return (
-    <div>
-      
+    <div className='main-bar'>
+        <div className='main-bar-header'>
+            {
+              // location.pathname === '/' ? <h1></h1>
+            }
+        </div>
     </div>
   )
 }
